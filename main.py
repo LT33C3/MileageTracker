e87c63cdb2b660ba89d62185a68be1f64beb8ddd
LT33C3 <113906719+LT33C3@users.noreply.github.com>
2024-07-08 21:55:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from kivy.app import App
 from kivy.uix.button import Button
+import os
+import sys
 
 class MileageTrackerApp(App):
     def build(self):
+        print(f"Python version: {sys.version}")
+        print(f"Kivy version: {App.version}")
+        print(f"Current working directory: {os.getcwd()}")
+        print(f"Files in current directory: {os.listdir('.')}")
         return Button(text='Hello World')
 
 if __name__ == '__main__':
